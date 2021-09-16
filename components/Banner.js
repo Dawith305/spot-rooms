06644fe96d44f8cd1aeac3015dd602234d264ae0
Dawit <dawit@Dawits-MacBook-Pro.local>
2021-09-16 22:21:56 +0300

    Banner Section Done

---
+++ components/Banner.js
@@ -0,0 +1,28 @@
+import Image from "next/image"
+
+
+function Banner() {
+    return (
+
+        // sm is greater than mobile so the defaut one are 
+        //for mobile the rest break points starting from sm 
+        //will be added to based on what we need 
+        <div className="relative h-[300px] sm:h-[400px]
+         lg:[500px] xl:[600px] xxl:[600px]">
+            <Image src="https://links.papareact.com/0fm" 
+            layout="fill"objectFit="cover"/>
+        
+            <div className="absolute top-1/2 w-full
+                     text-center">
+                <p className="text-sm sm:text-lg font-semibold">We have the perfect PLACE :)</p>
+                <button className="text-purple-500 bg-white px-10 py-4 shadow-md
+                                    rounded-full rounded-4 font-bold my-3 hover:shadow-xl
+                                    active:scale-90 transition duration-200
+                                    ">Take Me Away</button>
+            </div>
+        
+        </div>
+    )
+}
+
+export default Banner
